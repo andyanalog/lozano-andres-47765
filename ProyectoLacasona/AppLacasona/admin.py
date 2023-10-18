29bbaad6308a920b181ce1f7e7337915a82f4227
andyanalog <93492429+andyanalog@users.noreply.github.com>
2023-10-18 02:20:20 -0300

    Add files via upload

---
+++ ProyectoLacasona/AppLacasona/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(Newsletter)
+admin.site.register(AskAnything)
+admin.site.register(SendSong)
+admin.site.register(Avatar)
+admin.site.register(PublicacionDeImagen)
---
+++ ProyectoLacasona/AppLacasona/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django import forms
+# Create your models here.
+
+class Newsletter(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+
+class SendSong(models.Model):
+    songname = models.CharField(max_length=30)
+    email = models.EmailField(default="correo@ejemplo.com")
+    url = models.URLField(default="https://ejemplo.com")
+#    deadline = models.DateField()
+
+class AskAnything(models.Model):
+    message = models.CharField(max_length=100)
+    email = models.EmailField(default="correo@ejemplo.com")
+
+class Avatar(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to="avatares", null=True, blank=True)
+
+class PublicacionDeImagen(models.Model):
+    imagen = models.ImageField(upload_to='imagenes/')
+    descripcion = models.TextField()
+
